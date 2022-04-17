533974e372ba69c77fef2b71cfc9d9a55c5f486b
JonaThanPabonP <71296819+JonaThanPabonP@users.noreply.github.com>
2022-04-17 14:02:12 -0500

    Say hi using variables from path and set inside the HTML file

--- hola/urls.py
+++ hola/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.hola, name="hola"),
     path('html/', views.saludoHTML, name="saludoHTML"),
-    path('<str:nombre>/', views.chao, name="chao"),
+    # path('<str:nombre>/', views.chao, name="chao"),
+    path('<str:nombre>/', views.saludoVariableHTML, name="saludoVariableHTML"),
 ]
--- hola/views.py
+++ hola/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,8 +6,12 @@ from django.http import HttpResponse
 def hola(request):
     return HttpResponse("Hola Mundo!")
 
-def chao(request, nombre):
-    return HttpResponse(f"Chao {nombre}")
+# def chao(request, nombre):
+#     return HttpResponse(f"Chao {nombre}")
 
 def saludoHTML(request):
     return render(request, 'hola/index.html')
+
+def saludoVariableHTML(request, nombre):
+    context = {'name' : nombre}
+    return render(request, 'hola/saludar.html', context)
